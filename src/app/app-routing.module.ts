4814caf3719c0afc52c38f77307b9669e5d82636
Patryk Świerczewski <patryks456456@gmail.com>
2020-06-04 12:29:18 +0200

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { FirstPageComponent } from './components/first-page/first-page.component';
+import { SecondPageComponent } from './components/second-page/second-page.component';
+import { ThirdPageComponent } from './components/third-page/third-page.component';
+import { FourthPageComponent } from './components/fourth-page/fourth-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'page1', component: FirstPageComponent},
+  {path: 'page2', component: SecondPageComponent},
+  {path: 'page3', component: ThirdPageComponent},
+  {path: 'page4', component: FourthPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './components/first-page/first-page.component';
+import { SecondPageComponent } from './components/second-page/second-page.component';
+import { ThirdPageComponent } from './components/third-page/third-page.component';
+import { FourthPageComponent } from './components/fourth-page/fourth-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent,
+    FourthPageComponent
   ],
   imports: [
     BrowserModule,
