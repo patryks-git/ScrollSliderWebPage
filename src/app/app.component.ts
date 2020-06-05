403b02da5e776473ecb46dacc4df774acbbbac42
Patryk Świerczewski <patryks456456@gmail.com>
2020-06-05 10:39:47 +0200

    catching mouse wheel event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { slideInAnimation } from './route-animations';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { slideUpDownAnimation } from './route-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [slideInAnimation]
+  animations: [slideUpDownAnimation]
 })
 export class AppComponent {
+
   title = 'ScrollSliderWebPage';
+
+  @HostListener('mousewheel', ['$event']) onMouseWheelChrome(event: any) {
+    this.mouseWheelFunc(event);
+  }
+  mouseWheelFunc = (event: any) => {
+
+    if (event.wheelDelta >= 0) {
+      console.log("PageUp");
+    }
+
+    else if (event.wheelDelta < 0) {
+      console.log("PageDown");
+    }
+    else return;
+  }
 }
