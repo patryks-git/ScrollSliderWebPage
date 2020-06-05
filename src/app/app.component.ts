fafc36cbbcc6d7a7b47dadad18beab74eff6cb97
Patryk Świerczewski <patryks456456@gmail.com>
2020-06-05 11:25:16 +0200

    added onscroll event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Component, HostListener, ElementRef, OnInit } from '@angular/core';
 import { slideUpDownAnimation } from './route-animations';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,22 +8,50 @@ import { slideUpDownAnimation } from './route-animations';
   styleUrls: ['./app.component.scss'],
   animations: [slideUpDownAnimation]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router, private ref: ElementRef) { }
 
   title = 'ScrollSliderWebPage';
+
+  public components = ['/page1', '/page2', '/page3', '/page4'];
+  public currentComponent: number;
+
 
   @HostListener('mousewheel', ['$event']) onMouseWheelChrome(event: any) {
     this.mouseWheelFunc(event);
   }
+  @HostListener('DOMMouseScroll', ['$event']) onMouseWheelFirefox(event: any) {
+    this.mouseWheelFunc(event);
+  }
+  @HostListener('onmousewheel', ['$event']) onMouseWheelIE(event: any) {
+    this.mouseWheelFunc(event);
+  }
+
+  ngOnInit(): void {
+    this.router.navigate(['/page1']);
+    this.currentComponent = 0;
+  }
+
   mouseWheelFunc = (event: any) => {
+      if (event.wheelDelta >= 0) this.navigateToPrevious();
+      else if (event.wheelDelta < 0) this.navigateToNext();
+      else return;
+  }
 
-    if (event.wheelDelta >= 0) {
-      console.log("PageUp");
+  private navigateToNext() {
+    console.log("PageUp");
+    if (this.currentComponent + 1 < this.components.length) {
+      let path = this.components[++this.currentComponent];
+      this.router.navigate([path]);
     }
+  }
 
-    else if (event.wheelDelta < 0) {
-      console.log("PageDown");
+  private navigateToPrevious() {
+    console.log("PageDown");
+    if (this.currentComponent > 0) {
+      let path = this.components[--this.currentComponent];
+      this.router.navigate([path]);
     }
-    else return;
   }
 }
