5e3010bd25f908064ecb0e3a3b99e8a3e578feda
Patryk Świerczewski <patryks456456@gmail.com>
2020-06-06 15:42:45 +0200

    fixed start page route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
 import { slideUpDownAnimation } from './route-animations';
-import { Router} from '@angular/router';
+import { Router, NavigationEnd} from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +10,25 @@ import { Router} from '@angular/router';
   styleUrls: ['./app.component.scss'],
   animations: [slideUpDownAnimation]
 })
-export class AppComponent implements OnInit {
+
+export class AppComponent implements OnInit, OnDestroy {
 
   constructor(private router: Router) { }
 
   components = ['/page1', '/page2', '/page3', '/page4'];
   currentComponent: number;
-
+  routerSub: Subscription;
   private enableRouting = true;
 
-
   ngOnInit() {
-    this.currentComponent = this.components.indexOf(window.location.pathname);
+    this.routerSub = this.router.events
+      .pipe(filter(event => event instanceof(NavigationEnd)))
+      .subscribe((event: NavigationEnd) => {
+          this.currentComponent = this.components.indexOf(event.urlAfterRedirects);
+      });
+  }
+  ngOnDestroy() {
+    this.routerSub.unsubscribe();
   }
 
   animationDone($event) {
@@ -29,13 +38,14 @@ export class AppComponent implements OnInit {
   @HostListener('mousewheel', ['$event']) onMouseWheelChrome(event: any) {
     this.mouseWheelFunc(event);
   }
+
   @HostListener('DOMMouseScroll', ['$event']) onMouseWheelFirefox(event: any) {
     this.mouseWheelFunc(event);
   }
+
   @HostListener('onmousewheel', ['$event']) onMouseWheelIE(event: any) {
     this.mouseWheelFunc(event);
   }
-
 
   mouseWheelFunc = (event: any) => {
     if (this.enableRouting) {
@@ -51,7 +61,7 @@ export class AppComponent implements OnInit {
       this.enableRouting = false;
     }
   }
-
+  
   private navigateToPrevious() {
     if (this.currentComponent > 0) {
       let path = this.components[--this.currentComponent];
