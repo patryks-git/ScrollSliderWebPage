8d1d4d1f6d69f7d627d9fe1c3653ca584f55f637
Patryk Świerczewski <patryks456456@gmail.com>
2020-07-02 23:34:17 +0200

    Add ReactiveFormsModule and userForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { FourthPageComponent } from './components/fourth-page/fourth-page.compon
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/first-page/first-page.component.ts
+++ src/app/components/first-page/first-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from 'src/app/services/github.service';
 import { User } from 'src/app/models/user';
-import { map, filter } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-first-page',
@@ -11,11 +12,30 @@ import { map, filter } from 'rxjs/operators';
 export class FirstPageComponent implements OnInit {
 
   users: User[];
-  constructor(public githubService: GithubService) { }
+  userForm: FormGroup;
+  message = null;
+
+  constructor(
+    public githubService: GithubService,
+    public formBuilder: FormBuilder
+  ) { 
+    this.userForm = this.formBuilder.group({
+      login: ['', [ Validators.required ] ],
+      id: [null, [ Validators.required ] ],
+      type: [null, [ Validators.required ] ],
+      siteAdmin: [null, []]
+    });	
+
+    Object.keys(this.userForm.controls).forEach(controlName => {
+      this.userForm.get(controlName).valueChanges.subscribe(value => {
+        this.message = "You changed " + controlName;
+      });
+    });
+  }
 
   ngOnInit() {
     this.githubService.getUsers().pipe(
-      map(x => x.filter(y => y.login.startsWith("m")))
+      map(x => x.filter(y => y.login.endsWith("y")))
     ).subscribe(
       result => {
         this.users = result;
@@ -26,5 +46,43 @@ export class FirstPageComponent implements OnInit {
     )
   }
 
+  getUserInfo() {
+    // this.githubService.getUser(this.userForm.value.login).subscribe(
+    //   user => {
+    //     this.userForm.patchValue({
+    //       login: user.login,
+    //       id: user.id,
+    //       type: user.type,
+    //       siteAdmin: user.site_admin
+    //     });
+    //   },
+    //   error => {
+    //     console.error(error);
+    //   }
+    // )
+    let found = this.users.find(user => user.login === this.userForm.value.login);
+    if (found !== null) {
+      this.userForm.patchValue({
+        login: found.login,
+        id: found.id,
+        type: found.type,
+        siteAdmin: found.site_admin
+      });
+    }
+  }
 
+  addNewUser() {
+    if (this.userForm.valid) {
+      let newUser = this.userForm.value as User;
+      if (this.users.find(x => x.login === newUser.login || x.id === newUser.id)) {
+        this.message = "User with this login or id already exists."
+      } else {
+        this.users.push(newUser);
+        this.userForm.reset();
+        this.message = "User successfully created."
+      }
+    } else {
+      this.message = "Form is invalid. Fill all the required fields."
+    }
+  }
 }
